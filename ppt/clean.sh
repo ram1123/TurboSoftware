7ff1756917ae7c8d47e263d108740eef23eae2dd
Ram Krishna Sharma <ramkrishna.sharma71@gmail.com>
2015-01-25 18:37:31 +0100

    Improved RunTurbo script for Iterative alingment for all run range

--- ppt/clean.sh
+++ ppt/clean.sh
@@ -1,3 +1,4 @@
 rm AutoDict_vector_TFile__.cxx AutoDict_vector_TFile___cxx.d AutoDict_vector_TFile___cxx.so 
 rm OffsetFlip_EventBuilderVFAT_Oct2014_H2_Run1118AndUp.conf_Run1280_Ite*
 rm *.pdf *.txt
+rm fig_*.tex
