de755e3619fcf635e07461c893e46e22830a5648
Ram Krishna Sharma <ramkrishna.sharma71@gmail.com>
2015-04-17 21:22:28 +0200

    added setup for efficiency curve plotter

---
+++ EventBuilder/setup_eff_curve_maker.sh
@@ -0,0 +1,3 @@
+export SCRAM_ARCH=slc6_amd64_gcc481
+source /afs/cern.ch/sw/lcg/contrib/gcc/4.8/x86_64-slc6-gcc48-opt/setup.sh
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.20/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
