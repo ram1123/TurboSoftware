5ce1f1a91b882287e9e8c790bc9a9b5f119631b0
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2015-09-24 22:53:13 +0200

    Added Modifig Config scripts for below 1646 & above 1924

--- EventBuilder/ModifyConfig_BelowRun1646.sh
+++ EventBuilder/ModifyConfig_BelowRun1646.sh
@@ -11,5 +11,6 @@ awk -v OFS="\t" 'NR==FNR && NR==14 {a=$4} NR==FNR {next} FNR==14 {$2=$2-a} {prin
 awk -v OFS="\t" 'NR==FNR && NR==21 {a=$4} NR==FNR {next} FNR==7 {$3=$3-a} {print}' OffsetFile.log OffsetFlip_EventBuilderVFAT_NOV2014_H4.conf.buffer1 > OffsetFlip_EventBuilderVFAT_NOV2014_H4.conf.buffer2
 awk -v OFS="\t" 'NR==FNR && NR==22 {a=$4} NR==FNR {next} FNR==9 {$3=$3-a} {print}' OffsetFile.log OffsetFlip_EventBuilderVFAT_NOV2014_H4.conf.buffer2 > OffsetFlip_EventBuilderVFAT_NOV2014_H4.conf.buffer1
 
-#rm OffsetFlip_EventBuilderVFAT_NOV2014_H4.conf.buffer1
+cp OffsetFlip_EventBuilderVFAT_NOV2014_H4.conf.buffer1 ConfigFiles/OffsetFlip_EventBuilderVFAT_NOV2014_H4.conf
+rm OffsetFlip_EventBuilderVFAT_NOV2014_H4.conf.buffer1
 rm OffsetFlip_EventBuilderVFAT_NOV2014_H4.conf.buffer2
