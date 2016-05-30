bc0833f08eaa3c634a8bf5a5a9aafb6b6230e89c
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2016-05-30 12:46:55 +0200

    Modified Configuration file for run > 292

--- EventBuilder/RunTurbo.sh
+++ EventBuilder/RunTurbo.sh
@@ -450,6 +450,9 @@ do
   if [ $RunCounter -gt 238 ]; then
     cp Setting_EventBuilderVFAT_AboveRun238.conf Setting_EventBuilderVFAT.conf
   fi
+  if [ $RunCounter -gt 292 ]; then
+    cp Setting_EventBuilderVFAT_AboveRun292.conf Setting_EventBuilderVFAT.conf
+  fi
   #if [ $RunCounter -gt 1864 ]; then
   #  cp Setting_EventBuilderVFAT_AboveRun1864.conf Setting_EventBuilderVFAT.conf
   #  cp Setting_Analyzer_AboveRun1864.conf Setting_Analyzer.conf
